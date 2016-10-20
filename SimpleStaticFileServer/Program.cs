1c562ca32da860ddb0f0ffcf8f17c90548f09543
Passingwind <jxnkwlp@qq.com>
2016-10-20 10:00:44 +0800

    添加项目文件。

---
+++ SimpleStaticFileServer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace SimpleStaticFileServer
+{
+    class Program
+    {
+        public static string WebRoot;
+
+        static void Main(string[] args)
+        {
+            string root = GetEnterDirectory();
+
+            while (true)
+            {
+                if (Directory.Exists(root))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("The directory not found! ");
+                    Console.ResetColor();
+
+                    Console.WriteLine();
+
+                    root = GetEnterDirectory();
+                }
+            }
+
+            WebRoot = root;
+
+            var port = RandPort();
+
+            string url = string.Format("{0}:{1}", "http://127.0.0.1", port);
+
+            Console.WriteLine("Http Listener: " + url);
+            Console.WriteLine("Open it on you brower. ");
+
+            WebApp.Start(new StartOptions(url));
+
+            Console.ReadLine();
+
+        }
+
+        static string GetEnterDirectory()
+        {
+            Console.Write("Enter you web root directory: ");
+
+            var enter = Console.ReadLine();
+
+            return enter;
+        }
+
+        static int RandPort()
+        {
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+
+            return rand.Next(10000, 20000);
+        }
+    }
+}
---
+++ SimpleStaticFileServer/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using System.IO;
+
+namespace SimpleStaticFileServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Console.WriteLine();
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("[" + context.Response.StatusCode + "] " + context.Request.Uri);
+
+                await next();
+            });
+
+            if (!Directory.Exists(Program.WebRoot))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The directory '" + Program.WebRoot + "' not found!");
+                Console.ResetColor();
+
+                Console.WriteLine();
+
+                return;
+            }
+
+            var root = Program.WebRoot;
+
+            var fileSystem = new PhysicalFileSystem(root);
+
+            var fileServerOptions = new FileServerOptions()
+            {
+                FileSystem = fileSystem,
+                EnableDefaultFiles = true,
+                EnableDirectoryBrowsing = true,
+            };
+
+            app.UseFileServer(fileServerOptions);
+
+        }
+    }
+}
